297fde29e9464b049177e505c9dec3a21071d26e
lisseth <lisisotomonsalve@gmail.com>
2025-10-23 01:26:29 -0500

    arregl

--- src/Paciente/pages/Historial.jsx
+++ src/Paciente/pages/Historial.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import SideBar from "../components/SideBar";
-import { supabase } from "../supabaseClient";
+import supabase from "../../api/supabase";
 import {
   FaMapMarkerAlt,
   FaCalendarAlt,
